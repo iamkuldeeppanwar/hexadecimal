2f5997fb47e42735fe87fbdb6a549853d54cfaca
iamkuldeeppanwar <kuldeep10panwar@gmail.com>
2022-08-18 20:33:53 +0530

    bug fix

--- src/router/user.js
+++ src/router/user.js
@@ -3,8 +3,8 @@ const router = new express.Router();
 const userData = require("../data/userData");
 
 //Get All users
-router.get("/getUsers", function (req, res) {
-  res.json(userData);
+router.get("/getUsers", async function (req, res) {
+  await res.json(userData);
 });
 
 module.exports = router;
